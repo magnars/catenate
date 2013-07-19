3ea2ffed4bd65464293b5c2e1c28017ad07ca5fd
Magnar Sveen <magnars@gmail.com>
2013-07-19 08:46:25 +0200

    The arity function

--- src/catenate/core.clj
+++ src/catenate/core.clj
@@ -1,5 +1,11 @@
 (ns catenate.core)
 
+(defn arity [f]
+  (let [m (first (filter #(#{"invoke" "doInvoke"} (.getName %))
+                         (.getDeclaredMethods (class f))))
+        p (.getParameterTypes m)]
+    (alength p)))
+
 (defn foo
   "I don't do a whole lot."
   [x]
