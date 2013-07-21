1e3aad7e21f9a1957d42f22fa413e28148fb7a49
Magnar Sveen <magnars@gmail.com>
2013-07-21 10:04:33 +0200

    Add description and stuff to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject catenate "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Ring middleware to serve concatenated static files with cache buster URLs in production."
+  :url "http://github.com/magnars/catenate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[ring/ring-core "1.1.1"]
+                                  [hiccup "1.0.2"]]}})
